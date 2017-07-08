ffdbde39f10049b666f0491be312bd6bed84b524
Alexander Schieving <alexanderschieving@gmail.com>
2017-07-09 01:24:11 +0200

    Add song  validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,7 @@
 class Song < ApplicationRecord
+	belongs_to :artist
+
+	validates :title, presence: true, length: { maximum: 35}
+	validates :release_year, presence: true, length: { maximum: 4}
+
 end
