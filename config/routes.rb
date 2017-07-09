477b1a42861740e86584136427171070cc6f8144
Alexander Schieving <alexanderschieving@gmail.com>
2017-07-09 09:27:13 +0200

    Added links and cacade delete for artist

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "songs#index"
+  root "artists#index"
   
   resources :artists
   resources :songs
